058a884d27434577e74603347286f35e2686072a
Taariq Abdus-Samad <taariqasamad@gmail.com>
2026-02-09 22:03:53 -0500

    pseudonotes, outlining

---
+++ translator_app/main.py
@@ -0,0 +1,18 @@
+#Modules
+
+#Class
+    #Contructor
+
+    #App Object and Design
+
+    #button events
+
+    #translate click
+
+    #reset app
+
+    #translate text (google)
+
+    #reverse translation
+
+#Main Run
